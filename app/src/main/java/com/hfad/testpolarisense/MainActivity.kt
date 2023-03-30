4c34263a03bfb601949a18e01e9e846fa4f29551
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2023-03-30 19:09:57 +0300

    new

---
+++ app/src/main/java/com/hfad/testpolarisense/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hfad.testpolarisense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        val splashScreen = installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
