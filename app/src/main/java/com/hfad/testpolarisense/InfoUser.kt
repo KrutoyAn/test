974064619ea9695c27823ed961fd44b223d3ab06
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2023-04-08 21:12:23 +0300

    new

---
+++ app/src/main/java/com/hfad/testpolarisense/InfoUser.kt
@@ -0,0 +1,25 @@
+package com.hfad.testpolarisense
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class InfoUser : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_info_user, container, false)
+    }
+
+
+}
--- app/src/main/java/com/hfad/testpolarisense/MainActivity.kt
+++ app/src/main/java/com/hfad/testpolarisense/MainActivity.kt
@@ -3,18 +3,45 @@ package com.hfad.testpolarisense
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.hfad.testpolarisense.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val firstFragment = InfoUser()
+        val fm: FragmentManager = supportFragmentManager
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView(R.layout.activity_main)
 
+        setContentView(binding.root)
         splashScreen.setKeepOnScreenCondition{false}
 
 
+        binding.radioButton.setOnClickListener{
+            binding.radioButton.setBackgroundColor(R.drawable.button_selector)
+
+        }
+        binding.radioButton2.setOnClickListener{
+
+            binding.radioButton2.setBackgroundColor(R.drawable.button_selector)
+
+        }
+
+        binding.button3.setOnClickListener{
+            if (savedInstanceState == null) {
+            fm.beginTransaction()
+                .add(R.id.info_user, firstFragment)
+                .addToBackStack("myStack")
+                .commit()
+            }
+        }
+
     }
 }
+
